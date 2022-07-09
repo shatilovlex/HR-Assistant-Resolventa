5437c72df80a4edce1155d47fbca567988a82174
vicious <metalcors666@mail.ru>
2022-07-09 17:45:49 +0700

    #5 Create Crud Expectation Level

---
+++ src/Repository/ExpectationLevelRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ExpectationLevel;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ExpectationLevel>
+ *
+ * @method ExpectationLevel|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ExpectationLevel|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ExpectationLevel[]    findAll()
+ * @method ExpectationLevel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ExpectationLevelRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ExpectationLevel::class);
+    }
+
+    public function add(ExpectationLevel $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(ExpectationLevel $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return ExpectationLevel[] Returns an array of ExpectationLevel objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?ExpectationLevel
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
